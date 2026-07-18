a6e516011f2ec4e551a13479f26b9dbecdbca143
Aishaa <aisha@gmail.com>
2026-07-18 23:21:59 +0530

    Update Contact page success message text

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -325,7 +325,7 @@ export default function Contact() {
                 Message Sent!
               </h3>
               <p className="text-slate-500 text-sm leading-relaxed">
-               Your solar inquiry is shining bright in our inbox! <br /> for reaching out! Your solar inquiry is on its way to us. We'll contact you soon.
+               Your solar inquiry is shining bright in our inbox...Thanks for reaching out! We'll contact you soon!
               </p>
               <button 
                 onClick={() => setSuccess(false)}
